f9e103a6e0140e6339ca62ee28c4e957f0d8cd7e
Layla Codogno <layla.codogno@gmail.com>
2017-03-27 23:09:47 -0300

    Layout Base Navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,11 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
 //= require turbolinks
 //= require materialize-sprockets
 
 //= require_tree .
+
+$( document ).ready(function() {
+    $(".dropdown-button").dropdown();
+});
