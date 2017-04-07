788b8c9acd670bc9b1eb7df17acde7766ee70256
Layla Codogno <layla.codogno@gmail.com>
2017-04-06 22:07:26 -0300

    Added gravatar connection to project.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+	include Gravtastic
+	gravtastic
+
 	enum state: [
 		:AC, :AL, :AP, :AM, :BA, :CE, :DF, :ES, :GO, :MA, :MT, :MS, :MG, :PA, 
 		:PB, :PR, :PE, :PI, :RJ, :RN, :RS, :RO, :RR, :SC, :SP, :SE, :TO
