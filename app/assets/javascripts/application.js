aa68a0d9206b2b52515211afd32b2d2257b8de49
Layla Codogno <layla.codogno@gmail.com>
2017-04-09 17:56:42 -0300

    code for input label not working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,18 +1,5 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require materialize-sprockets
-
 //= require_tree .
 
 $( document ).ready(function() {
@@ -20,4 +7,6 @@ $( document ).ready(function() {
     $("main").css("min-height", windowHeight);
 
     $(".dropdown-button").dropdown();
+
+    $(".input-field label").addClass("active");
 });
