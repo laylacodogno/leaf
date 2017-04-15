8530025aa1abaa4a8cc77e1e94602e98e28e1587
Layla Codogno <layla.codogno@gmail.com>
2017-04-15 17:47:17 -0300

    Ingredient frist validation

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,3 @@
 class Ingredient < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3, too_short: "ops nome muito pequeno. deve ter no mínimo 3 caracteres." }, uniqueness: {message: "ops, esse ingrediente já existe."}
 end
