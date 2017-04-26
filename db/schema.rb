14373f8961c2a5236c2fa9884d7eb52c946c7d2d
Layla Codogno <layla.codogno@gmail.com>
2017-04-25 23:01:50 -0300

    Added common categories table to project.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170425232821) do
+ActiveRecord::Schema.define(version: 20170426015849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.string   "normalized_name"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_categories_on_user_id", using: :btree
+  end
+
+  create_table "common_categories", force: :cascade do |t|
     t.string   "name"
     t.string   "normalized_name"
     t.datetime "created_at",      null: false
@@ -47,5 +56,6 @@ ActiveRecord::Schema.define(version: 20170425232821) do
     t.string   "login",      null: false
   end
 
+  add_foreign_key "categories", "users"
   add_foreign_key "ingredients", "users"
 end
