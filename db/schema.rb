a3bb0c188dbe2b0f505e5b120a0f8ddcd6fb4d32
Layla Codogno <layla.codogno@gmail.com>
2017-05-01 22:47:18 -0300

    Layout to devise screen

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426014527) do
+ActiveRecord::Schema.define(version: 20170426015849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.string   "normalized_name"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_categories_on_user_id", using: :btree
+  end
+
+  create_table "common_categories", force: :cascade do |t|
     t.string   "name"
     t.string   "normalized_name"
     t.datetime "created_at",      null: false
@@ -58,5 +67,6 @@ ActiveRecord::Schema.define(version: 20170426014527) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "categories", "users"
   add_foreign_key "ingredients", "users"
 end
