185beba20968700aa496f73ec1375034dfb43ff5
Layla Codogno <layla.codogno@gmail.com>
2017-05-15 22:41:00 -0300

    Added redirect to rooth url after login, logout and signup.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,16 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :state])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :state])
   end
+
+	def after_sign_in_path_for(resource_or_scope)
+		root_path
+	end
+
+	def after_sign_out_path_for(resource_or_scope)
+		root_path
+	end
+	
+	def after_sign_up_path_for(resource_or_scope)
+		root_path
+	end
 end
