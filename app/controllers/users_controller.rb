5901d65c58d4582c784fcffb7786629f1a6fa429
Layla Codogno <layla.codogno@gmail.com>
2017-05-16 22:44:24 -0300

    Removed index and new user routes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,37 +2,12 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
 
-  # GET /users
-  def index
-    @users = User.all
-  end
-
   # GET /users/1
   def show
   end
 
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-
   # GET /users/1/edit
   def edit
-  end
-
-  # POST /users
-  def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'Usuário salvo com sucesso!' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PATCH/PUT /users/1
