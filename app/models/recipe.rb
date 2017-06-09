a793e4b5018a016ece31713806da48b59bdbb815
Layla Codogno <layla.codogno@gmail.com>
2017-06-08 22:14:59 -0300

    Added recipe relation with user.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,7 @@
 class Recipe < ApplicationRecord
+	belongs_to :user
+
+	validates :title, presence: true, length: { minimum: 3, unless: "title.blank?" }
+	validates :directions, presence: true, length: { minimum: 15, unless: "directions.blank?" }
+	validates :servings, numericality: { greater_than_or_equal_to: 1, unless: "servings.blank?" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,88 +10,90 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170602015309) do
+ActiveRecord::Schema.define(version: 20170609004510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
-  t.string   "name",      null: false
-  t.string   "normalized_name", null: false
-  t.datetime "created_at",    null: false
-  t.datetime "updated_at",    null: false
-  t.integer  "user_id"
-  t.index ["user_id"], name: "index_categories_on_user_id", using: :btree
+    t.string   "name",            null: false
+    t.string   "normalized_name", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_categories_on_user_id", using: :btree
   end
 
   create_table "common_categories", force: :cascade do |t|
-  t.string   "name",      null: false
-  t.string   "normalized_name", null: false
-  t.datetime "created_at",    null: false
-  t.datetime "updated_at",    null: false
+    t.string   "name",            null: false
+    t.string   "normalized_name", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "common_ingredients", force: :cascade do |t|
-  t.string   "name",      null: false
-  t.string   "normalized_name", null: false
-  t.datetime "created_at",    null: false
-  t.datetime "updated_at",    null: false
+    t.string   "name",            null: false
+    t.string   "normalized_name", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "ingredients", force: :cascade do |t|
-  t.string   "name",      null: false
-  t.string   "normalized_name", null: false
-  t.datetime "created_at",    null: false
-  t.datetime "updated_at",    null: false
-  t.integer  "user_id"
-  t.index ["user_id"], name: "index_ingredients_on_user_id", using: :btree
+    t.string   "name",            null: false
+    t.string   "normalized_name", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_ingredients_on_user_id", using: :btree
   end
 
   create_table "measurement_unit_conversions", force: :cascade do |t|
-  t.integer  "from_measurement_unit_id"
-  t.integer  "to_measurement_unit_id"
-  t.float  "factor"
-  t.datetime "created_at",         null: false
-  t.datetime "updated_at",         null: false
-  t.index ["from_measurement_unit_id"], name: "index_measurement_unit_conversions_on_from_measurement_unit_id", using: :btree
-  t.index ["to_measurement_unit_id"], name: "index_measurement_unit_conversions_on_to_measurement_unit_id", using: :btree
+    t.integer  "from_measurement_unit_id"
+    t.integer  "to_measurement_unit_id"
+    t.float    "factor"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.index ["from_measurement_unit_id"], name: "index_measurement_unit_conversions_on_from_measurement_unit_id", using: :btree
+    t.index ["to_measurement_unit_id"], name: "index_measurement_unit_conversions_on_to_measurement_unit_id", using: :btree
   end
 
   create_table "measurement_units", force: :cascade do |t|
-  t.string   "name",        null: false
-  t.integer  "measurement_unit_id"
-  t.datetime "created_at",      null: false
-  t.datetime "updated_at",      null: false
-  t.index ["measurement_unit_id"], name: "index_measurement_units_on_measurement_unit_id", using: :btree
+    t.string   "name",                null: false
+    t.integer  "measurement_unit_id"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.index ["measurement_unit_id"], name: "index_measurement_units_on_measurement_unit_id", using: :btree
   end
 
   create_table "recipes", force: :cascade do |t|
-  t.string   "title",      null: false
-  t.time   "preparation_time"
-  t.integer  "servings"
-  t.string   "directions",     null: false
-  t.datetime "created_at",     null: false
-  t.datetime "updated_at",     null: false
+    t.string   "title",            null: false
+    t.time     "preparation_time"
+    t.integer  "servings"
+    t.string   "directions",       null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_recipes_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
-  t.string   "name",                null: false
-  t.string   "email",                 null: false
-  t.datetime "created_at",              null: false
-  t.datetime "updated_at",              null: false
-  t.string   "username"
-  t.integer  "state"
-  t.string   "encrypted_password",   default: "", null: false
-  t.string   "reset_password_token"
-  t.datetime "reset_password_sent_at"
-  t.datetime "remember_created_at"
-  t.integer  "sign_in_count",      default: 0,  null: false
-  t.datetime "current_sign_in_at"
-  t.datetime "last_sign_in_at"
-  t.inet   "current_sign_in_ip"
-  t.inet   "last_sign_in_ip"
-  t.index ["email"], name: "index_users_on_email", using: :btree
-  t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.string   "name",                                null: false
+    t.string   "email",                               null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "username"
+    t.integer  "state"
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.index ["email"], name: "index_users_on_email", using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   add_foreign_key "categories", "users"
@@ -99,4 +101,5 @@ ActiveRecord::Schema.define(version: 20170602015309) do
   add_foreign_key "measurement_unit_conversions", "measurement_units", column: "from_measurement_unit_id"
   add_foreign_key "measurement_unit_conversions", "measurement_units", column: "to_measurement_unit_id"
   add_foreign_key "measurement_units", "measurement_units"
+  add_foreign_key "recipes", "users"
 end
