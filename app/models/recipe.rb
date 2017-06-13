c597a3ebeae650abed6742fbc5360bf9881877c6
Layla Codogno <layla.codogno@gmail.com>
2017-06-13 01:08:49 -0300

    Added category validation and preparation time right format in form.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -8,4 +8,5 @@ class Recipe < ApplicationRecord
 	validates :title, presence: true, length: { minimum: 3, unless: "title.blank?" }
 	validates :directions, presence: true, length: { minimum: 15, unless: "directions.blank?" }
 	validates :servings, numericality: { greater_than_or_equal_to: 1, unless: "servings.blank?" }
+	validates :categories, presence: true
 end
