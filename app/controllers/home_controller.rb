79dfbb62db4d552ea0b282cbd08e2b6a9ffff4c1
Tabata Ruiz <tabata@tabataruiz.com.br>
2017-06-18 17:34:21 -0300

    Added home and categories list

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
   def index
+    @recipes = Recipe.order('created_at desc')
   end
+
+  def search
+    @categories = Category.all
+    @recipes = Recipe.order('created_at desc')
+
+    if params[:category_ids].present?
+      @recipes = @recipes.includes(:categories).where(categories: {id: params[:category_ids]})
+    end
+
+  end
+
 end
